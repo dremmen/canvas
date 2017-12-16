05d3be0739b339250f9546d77b512728caa3ad20
dremmen <1437656519@qq.com>
2017-12-16 11:18:52 +0800

    一个简单的css3翻页

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(function(){
+
+	var main = $('#main'),
+		page = $('.page'),
+		prev = $('.prev'),
+		next = $('.next'),
+		pL = page.length,
+		index = 0;
+		
+		next.click(function(){
+			page.eq(index).removeClass('hide').addClass('show');
+			index++;
+			if(index>pL){
+				index = pL;
+			}
+			console.log(index);
+		})
+
+		prev.click(function(){
+			page.eq(index).removeClass('show').addClass('hide');
+			index--;
+			if(index<0){
+				index = 0;
+				page.removeClass('show').addClass('hide');
+			}
+			console.log(index);
+		})
+
+	
+})
